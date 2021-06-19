1fb70b09f062e41b12ed8efa6c5dae305421a356
creatorckim <calvinkim4@yahoo.com>
2021-06-19 00:53:22 -0400

    added random array generator

--- src/Visualizer/Visualizer.jsx
+++ src/Visualizer/Visualizer.jsx
@@ -6,8 +6,19 @@ export default class Visualizer extends Component {
         super(props);
 
         this.state = {
-            numberOfBars: 50
+            numberOfBars: 50,
+            randomArray: []
         };
+    }
+
+    randomizeArray = () => {
+        let array = [];
+        for (let i = 0; i < this.state.numberOfBars; i++) {
+            array.push(Math.floor((Math.random() * 100) + 1));
+        }
+        this.setState({
+            randomArray: array
+        });
     }
 
     valueChange = (evt) => {
@@ -17,12 +28,19 @@ export default class Visualizer extends Component {
 
     }
 
-
     render() {
+        const {randomArray} = this.state;
+
         return (
-            <div>
+            <div id='parent-container'>
                 <h1>{this.state.numberOfBars}</h1>
                 <input type= 'range' min= '3' max= '100' value= {this.state.numberOfBars} onChange= {this.valueChange}/>
+                <button onClick={this.randomizeArray}>Get Random Array</button>
+                <div className='array-container'>
+                    {randomArray.map((value, idx) => (
+                        <div className='random-div' key={idx} style={{height: `${value}%`}}></div>
+                    ))}
+                </div>
             </div>
             
         )
