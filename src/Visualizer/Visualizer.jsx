0b47b404e314bfedeacd666a769aeda53feaa3b2
creatorckim <calvinkim4@yahoo.com>
2021-06-18 23:45:04 -0400

    added value bar

--- src/Visualizer/Visualizer.jsx
+++ src/Visualizer/Visualizer.jsx
@@ -1,11 +1,30 @@
-import { Component}  from 'react';
+import { Component }  from 'react';
 import './Visualizer.css';
 
 export default class Visualizer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numberOfBars: 50
+        };
+    }
+
+    valueChange = (evt) => {
+        this.setState({
+            numberOfBars: evt.target.value
+        })
+
+    }
+
 
     render() {
         return (
-            <h1>test 2</h1>
+            <div>
+                <h1>{this.state.numberOfBars}</h1>
+                <input type= 'range' min= '3' max= '100' value= {this.state.numberOfBars} onChange= {this.valueChange}/>
+            </div>
+            
         )
     }
 }
