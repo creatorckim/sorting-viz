5623dc69df966c20855d7e0f76b35bab4eac90b6
creatorckim <calvinkim4@yahoo.com>
2021-06-19 21:24:56 -0400

    Changed visualizer to function component

---
+++ src/SortingAlgorithms/BubbleSort.jsx
@@ -0,0 +1,22 @@
+function BubbleSort(array) {
+    let turnsArray = [];
+    let unsortedArray = array;
+    for (let i = 0; i < unsortedArray.length; i++) {
+        for (let j = 0; j < (unsortedArray.length - i - 1); j++) {
+            if (unsortedArray[j] > unsortedArray[j + 1]) {
+                turnsArray.push([j + 1, j]);
+                let temp = unsortedArray[j];
+                unsortedArray[j] = unsortedArray[j + 1];
+                unsortedArray[j + 1] = temp;
+            } else {
+                turnsArray.push([j, j + 1]);
+
+            }
+
+        }
+    }
+
+    return unsortedArray;
+}
+
+export default BubbleSort;
--- src/Visualizer/Visualizer.jsx
+++ src/Visualizer/Visualizer.jsx
@@ -1,50 +1,156 @@
-import { Component }  from 'react';
+import React from 'react';
 import './Visualizer.css';
+import BubbleSort from '../SortingAlgorithms/BubbleSort';
+const { useState, useEffect } = React;
 
-export default class Visualizer extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            numberOfBars: 50,
-            randomArray: []
-        };
+const Visualizer = () => {
+    const [numberOfBars, setNumberOfBars] = useState(50);
+    const [randomArray, setRandomArray] = useState([]);
+
+    const valueChange = (evt) => {
+        setNumberOfBars(evt.target.value);
     }
 
-    randomizeArray = () => {
+    const randomizeArray = () => {
         let array = [];
-        for (let i = 0; i < this.state.numberOfBars; i++) {
+        for (let i = 0; i < numberOfBars; i++) {
             array.push(Math.floor((Math.random() * 100) + 1));
         }
-        this.setState({
-            randomArray: array
-        });
+        setRandomArray(array);
     }
 
-    valueChange = (evt) => {
-        this.setState({
-            numberOfBars: evt.target.value
-        })
-
+    const updateSortedArray = () => {
+        let array = BubbleSort(randomArray);
+        setRandomArray(Object.values(array));
     }
 
-    render() {
-        const {randomArray} = this.state;
+    return (
+        <div id='parent-container'>
+            <header className='header'>
+                <h1>{numberOfBars}</h1>
+                <input type= 'range' min= '3' max= '100' value= {numberOfBars} onChange= {(evt) => {valueChange(evt)}}/>
+                <button onClick={() => {randomizeArray()}}>Randomize</button>
+                <button onClick={() => {updateSortedArray()}}>BubbleSort</button>
+            </header>
+            <div className='array-container'>
+                {randomArray.map((value, idx) => (
+                <div className='random-div' key={idx} style={{height: `${value}%`}}></div>
+            ))}
+        </div>
+    </div>
+    )
 
-        return (
-            <div id='parent-container'>
-                <header className='header'>
-                    <h1>{this.state.numberOfBars}</h1>
-                    <input type= 'range' min= '3' max= '100' value= {this.state.numberOfBars} onChange= {this.valueChange}/>
-                    <button onClick={this.randomizeArray}>Randomize</button>
-                </header>
-                <div className='array-container'>
-                    {randomArray.map((value, idx) => (
-                        <div className='random-div' key={idx} style={{height: `${value}%`}}></div>
-                    ))}
-                </div>
-            </div>
-            
-        )
-    }
 }
+
+export default Visualizer;
+
+
+
+
+
+// import { Component }  from 'react';
+// import './Visualizer.css';
+// import bubbleSort from '../SortingAlgorithms/BubbleSort';
+
+// export default class Visualizer extends Component {
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {
+//             numberOfBars: 50,
+//             randomArray: [],
+//             turnsArray: [],
+//             interval: false,
+//             count: 0
+//         };
+//     }
+
+//     componentDidMount() {
+//         this.randomizeArray();
+//     }
+
+//     randomizeArray = () => {
+//         let array = [];
+//         for (let i = 0; i < this.state.numberOfBars; i++) {
+//             array.push(Math.floor((Math.random() * 100) + 1));
+//         }
+//         this.setState({
+//             randomArray: array,
+//             count: 0
+//         });
+
+//     }
+
+//     valueChange = (evt) => {
+//         this.setState({
+//             numberOfBars: evt.target.value
+//         })
+//     }
+
+//     updateState = (array) => {
+//         //this updates randomArray as well
+//         this.setState({
+//             turnsArray: array
+//         })
+
+//         //console.log(array);
+
+//         // this.setState({
+//         //     interval: true
+//         // })
+
+//         for (let i = 0; i < array.length; i++) {
+//             let divArray = document.getElementsByClassName('random-div');
+//             // let firstBarHeight = divArray[array[i][0]].style.height;
+//             // let secondBarHeight = divArray[array[i][1]].style.height;
+//             // let [firstIdx, secondIdx] = array[i];
+//             // if (firstIdx > secondIdx) {
+//             //     let tempHeight = divArray[firstIdx].style.height;
+//             //     divArray[firstIdx].style.height = divArray[secondIdx].style.height;
+//             //     divArray[secondIdx].style.height = tempHeight;
+
+//             // }
+            
+//             // setTimeout(() => {
+//             //     let [firstIdx, secondIdx] = array[i];
+//             //     if (firstIdx > secondIdx) {
+//             //         // firstBar.backgroundColor = 'black';
+//             //         // secondBar.backgroundColor = 'white';
+//             //         // let tempHeight = firstBarHeight;
+//             //         // let firstBarHeight = secondBarHeight;
+//             //         // let secondBarHeight = tempHeight;
+
+//             //         let tempHeight = divArray[firstIdx].style.height;
+//             //         divArray[firstIdx].style.height = divArray[secondIdx].style.height;
+//             //         divArray[secondIdx].style.height = tempHeight;
+
+//             //     }
+                
+//             // }, 2000);
+
+//         }
+
+//     }
+
+//     render() {
+//         const {randomArray, numberOfBars} = this.state;
+
+//         return (
+//             <div id='parent-container'>
+//                 <header className='header'>
+//                     <h1>{numberOfBars}</h1>
+//                     <input type= 'range' min= '3' max= '100' value= {numberOfBars} onChange= {this.valueChange}/>
+//                     <button onClick={this.randomizeArray}>Randomize</button>
+//                     <button onClick={() => {this.updateState(bubbleSort(randomArray))}}>BubbleSort</button>
+//                 </header>
+//                 <div className='array-container'>
+//                     {randomArray.map((value, idx) => (
+//                         <div className='random-div' key={idx} style={{height: `${value}%`}}></div>
+//                     ))}
+//                 </div>
+//             </div>
+            
+//         )
+//     }
+// }
